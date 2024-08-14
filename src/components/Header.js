e6987c16c1b3ecc782f5b3712b8dd327c7b4645c
caytaonohoa1609 <164864433+caytaonohoa1609@users.noreply.github.com>
2024-08-14 15:30:16 +0700

    #24 Count Users - Chia Sẻ Data Redux (Tương Tự Đếm items trong Cart)

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,11 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useSelector } from 'react-redux';
 
 const Header = (props) => {
+
+    const listUsers = useSelector((state) => state.user.listUsers)
     return (
         <>
             <Navbar bg="light" collapseOnSelect expand="lg" className="bg-body-tertiary">
@@ -12,26 +15,26 @@ const Header = (props) => {
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="me-auto">
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
-                    <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">
-                        Another action
-                    </NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">
-                        Separated link
-                    </NavDropdown.Item>
+                    <NavDropdown 
+                        style={{marginLeft: "50px"}} 
+                        title={`(${listUsers.length}) Users`}
+                        id="collapsible-nav-dropdown"
+                    >
+                        {
+                            listUsers && listUsers.length > 0 &&
+                            listUsers.map((item, index) => {
+                                return (
+                                    <NavDropdown.Item href="#">
+                                        {item.email}
+                                    </NavDropdown.Item>
+                                )
+                            })
+                        }
+                    
+                    
                     </NavDropdown>
                 </Nav>
-                <Nav>
-                    <Nav.Link href="#deets">More deets</Nav.Link>
-                    <Nav.Link eventKey={2} href="#memes">
-                    Dank memes
-                    </Nav.Link>
-                </Nav>
+                
                 </Navbar.Collapse>
             </Container>
             </Navbar>
